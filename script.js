0890c2963c9898b2351b3616f41031b578f83f5f
Eric Pezzulo <eric.pezzulo@gmail.com>
2021-04-26 12:54:06 -0400

    all finished

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
 const player1Btn = document.querySelector('#player1btn');
 const player2Btn = document.querySelector('#player2btn');
-const reset = document.querySelector('#reset');
+const resetButton = document.querySelector('#reset');
 const roundSelect = document.querySelector('#roundNum');
 
-const player1Score = document.querySelector('#currentScoreP1');
-const player2Score = document.querySelector('#currentScoreP2');
+const player1Display = document.querySelector('#p1Display');
+const player2Display = document.querySelector('#p2Display');
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
 
 
-player1Btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    updatedP1Score = player1Score.innerText++;
+player1Btn.addEventListener('click', () => {
+
+    if (!isGameOver){
+        p1Score +=1;
+        if (p1Score === winningScore){
+            isGameOver = true;
+            player1Display.classList.add('winner');
+            player2Display.classList.add('loser');
+        }
+        player1Display.textContent = p1Score;
+    }
 });
 
-player2Btn.addEventListener('click', function (e) {
-    e.preventDefault();
-    updatedP2Score = player2Score.innerText++;
+player2Btn.addEventListener('click', () => {
+    if (!isGameOver){
+        p2Score +=1;
+        if (p2Score === winningScore){
+            isGameOver = true;
+            player2Display.classList.add('winner');
+            player1Display.classList.add('loser');
+        }
+        player2Display.textContent = p2Score;
+    }
 });
 
-reset.addEventListener('click', (e) => {
-    e.preventDefault();
-    resetScore = player1Score.innerText='0';
-    resetScore = player2Score.innerText='0';
+roundSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
 })
 
-// roundSelect.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     roundsSelected = roundSelect.value;
-//     // console.log(roundSelect.value)
-//     return roundsSelected;
-// })
+resetButton.addEventListener('click', reset)
 
-const findLimit = () => {
-
-    
-    let roundLimit = roundSelect.options[roundSelect.selectedIndex];
-    roundSelect.addEventListener('click', (e) => {
-        e.preventDefault();
-        roundLimit = roundSelect.value;
-        console.log(roundLimit);
-        return roundLimit;
-    })
-    
-    return roundLimit;
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    resetScore = player1Display.textContent='0';
+    resetScore = player2Display.textContent='0';
+    player1Display.classList.remove('winner', 'loser');
+    player2Display.classList.remove('winner', 'loser');
 }
-findLimit();
 
-// const calculateWinner = (roundsSelected) => {
-//     roundSelect.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         roundsSelected = roundSelect.value;
-//         console.log(roundSelect.value)
-//         return roundsSelected;
-//     })
-//     if (player1Score.innerText = roundsSelected) {
-//         updatedP1Score = roundsSelected;
-//     } else if (player2Score.innerText = roundsSelected) {
-//         updatedP2Score = roundsSelected;
-//     }
-// }
-// calculateWinner();
+
