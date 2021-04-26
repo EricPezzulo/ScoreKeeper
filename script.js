9db1e2921949b8d75227df0db54f68fc07a4aa65
Eric Pezzulo <eric.pezzulo@gmail.com>
2021-04-25 21:43:07 -0400

    trying alternate solutions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const player1Btn = document.querySelector('#player1btn');
 const player2Btn = document.querySelector('#player2btn');
 const reset = document.querySelector('#reset');
+const roundSelect = document.querySelector('#roundNum');
 
 const player1Score = document.querySelector('#currentScoreP1');
 const player2Score = document.querySelector('#currentScoreP2');
@@ -10,7 +11,6 @@ player1Btn.addEventListener('click', function (e) {
     e.preventDefault();
     updatedP1Score = player1Score.innerText++;
 });
-
 
 player2Btn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -22,3 +22,40 @@ reset.addEventListener('click', (e) => {
     resetScore = player1Score.innerText='0';
     resetScore = player2Score.innerText='0';
 })
+
+// roundSelect.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     roundsSelected = roundSelect.value;
+//     // console.log(roundSelect.value)
+//     return roundsSelected;
+// })
+
+const findLimit = () => {
+
+    
+    let roundLimit = roundSelect.options[roundSelect.selectedIndex];
+    roundSelect.addEventListener('click', (e) => {
+        e.preventDefault();
+        roundLimit = roundSelect.value;
+        console.log(roundLimit);
+        return roundLimit;
+    })
+    
+    return roundLimit;
+}
+findLimit();
+
+// const calculateWinner = (roundsSelected) => {
+//     roundSelect.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         roundsSelected = roundSelect.value;
+//         console.log(roundSelect.value)
+//         return roundsSelected;
+//     })
+//     if (player1Score.innerText = roundsSelected) {
+//         updatedP1Score = roundsSelected;
+//     } else if (player2Score.innerText = roundsSelected) {
+//         updatedP2Score = roundsSelected;
+//     }
+// }
+// calculateWinner();
